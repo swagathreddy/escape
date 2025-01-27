4ea0bc5ea7c520a8341769ed83c5b3a145c94e1e
Swagath reddy <125737432+swagathreddy@users.noreply.github.com>
2025-01-27 21:38:14 +0530

    admin.py

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Element,Theme,Room,Answer
+from .models import Element,Theme,Room,Answer,UserGameSession
 admin.site.register(Element)
 admin.site.register(Theme)
 admin.site.register(Room)
 admin.site.register(Answer)
+admin.site.register(UserGameSession)
